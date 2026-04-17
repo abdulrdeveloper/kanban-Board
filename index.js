f338082de60e8846d42b801cf0b5ed2024ad5693
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-04-17 11:01:42 +0500

    Refactor click event listener for task management functionality

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import TaskManager from "./taskManager.js";
 
 const columns = document.querySelectorAll(".column");
 const tasks = document.querySelectorAll(".tasks");
-document.addEventListener("click", (event) => {
 
+document.addEventListener("click", (event) => {
     if (event.target.classList.contains("add-btn")) {
-        if (!document.querySelector(".containerofgetUserTask")) {
+        if (!document.querySelector(".containerofgetUserTask")) {          
             const taskManager = new TaskManager(event.target.closest(".column"));
             taskManager.getUserData();
             taskManager.addBtnofData();
             taskManager.cancelBtnOfData();
         }
     }
-})
+});
